87880f399b71155230455ae6195c801a6862c721
Excellence D. <eoyeniran@tradedepot.co>
2025-04-24 15:30:07 +0100

    chore: rebuild recipes page

--- src/pages/recipes.js
+++ src/pages/recipes.js
@@ -1,21 +1,49 @@
 import { Header } from "../components/header";
 import CookedFoodOne from "../assets/processed-food.png";
 import CookedFoodTwo from "../assets/fish-rice-removebg-preview.png";
+import ChefTwo from "../assets/chef-two.png";
 
 export function Recipes() {
   return (
     <div className="recipes">
       <Header />
+      <div className="hero-block">
+        <img src={ChefTwo} alt="chef-look" />
+        <h3>Delicious Recipes Made With Love</h3>
+        <h6>
+          From Jollof to Beans Porridge to Eba, explore vibrant recipes crafted
+          with Mangrove Foods.
+        </h6>
+        <button>Submit your recipe</button>
+      </div>
       <div className="recipes-container">
-        <h1 className="page-title">Our Recipes</h1>
+        <h1 className="page-title">Explore Our Recipes</h1>
         <ul className="recipe-categories">
           <li className="active">Cereal</li>
           <li>Rice Recipes</li>
           <li>Yam Recipes</li>
           <li>Canned Foods</li>
         </ul>
+        <div className="recipes-flex-2">
+          {[1, 2, 3, 4, 5, 6, 6, 3, 2, 2].map((el, key) => (
+            <div className="recipe-box" key={key}>
+              <div className={`recipe-box-img ${el % 2 ? "fish" : ""}`}>
+                <img
+                  src={el % 2 ? CookedFoodTwo : CookedFoodOne}
+                  alt="recipe"
+                  className={`img-fluid`}
+                />
+              </div>
+              <div className="recipe-box-content">
+                <h3>Jollof Rice</h3>
+                <p>A delicious beef african classic</p>
+                <button>View Recipe</button>
+              </div>
+            </div>
+          ))}
+        </div>
 
-        <div className="recipes-flex">
+        {/* <div className="recipes-flex">
           {[1, 2, 3, 4, 5, 6, 2, 4, 2, 3, 2].map((el, key) => (
             <div className="recipe-box" key={key}>
               <div className={`recipe-box-img ${el % 2 ? "fish" : ""}`}>
@@ -40,7 +68,7 @@ export function Recipes() {
               </button>
             </div>
           ))}
-        </div>
+        </div> */}
       </div>
     </div>
   );
