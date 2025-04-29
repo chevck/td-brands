c98dd439ed45c30e2e96e9d5b85f94def85d57e1
Excellence D. <eoyeniran@tradedepot.co>
2025-04-29 16:41:07 +0100

    fix: added footer linking for about-us

--- src/components/footer.js
+++ src/components/footer.js
@@ -5,6 +5,7 @@ import { useState } from "react";
 
 export function Footer() {
   const [emailAddress, setEmailAddress] = useState("");
+  const isOnHomePage = window.location.pathname === "/";
 
   const isEmailValid = (email) => {
     const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -18,17 +19,17 @@ export function Footer() {
   };
 
   return (
-    <div className="footer-container">
-      <div className="footer">
-        <div className="logo-column" onClick={() => window.location.href("/")}>
-          <img src={MangroveLogo} alt="Mangrove Logo" />
+    <div className='footer-container'>
+      <div className='footer'>
+        <div className='logo-column' onClick={() => window.location.href("/")}>
+          <img src={MangroveLogo} alt='Mangrove Logo' />
         </div>
-        <div className="height-line" />
-        <div className="menu-column">
+        <div className='height-line' />
+        <div className='menu-column'>
           <ul>
             <li>
               <a
-                href="#about-us"
+                href={isOnHomePage ? "#about-us" : "/#about-us"}
                 onClick={() => trackEvent("Clicked About Us")}
               >
                 About Us
@@ -36,60 +37,60 @@ export function Footer() {
             </li>
             <li>
               <a
-                href="#products-range"
+                href={isOnHomePage ? "#products-range" : "/#products-range"}
                 onClick={() => trackEvent("Clicked Products")}
               >
                 products
               </a>
             </li>
             <li>
-              <a href="mailto:media@mangrovefoods.com">Contact Us</a>
+              <a href='mailto:media@mangrovefoods.com'>Contact Us</a>
             </li>
             <li>
-              <a href="/privacy-terms?page=privacy">Privacy Policy</a>
+              <a href='/privacy-terms?page=privacy'>Privacy Policy</a>
             </li>
             <li>
-              <a href="/privacy-terms?page=terms">Terms and Conditions</a>
+              <a href='/privacy-terms?page=terms'>Terms and Conditions</a>
             </li>
           </ul>
         </div>
-        <div className="height-line" />
-        <div className="newsletter-column">
+        <div className='height-line' />
+        <div className='newsletter-column'>
           <h5>Stay up to date</h5>
           <p>
             Sign up for our newsletter and we’ll keep you up to date on
             everything from the Mangrove range
           </p>
           <input
-            className="form-control"
-            type="text"
-            placeholder="Enter your email"
+            className='form-control'
+            type='text'
+            placeholder='Enter your email'
             onChange={({ target: { value } }) => setEmailAddress(value)}
           />
           <button onClick={handleSubscribeUser}>
             <p>Subscribe</p>
-            <i className="bi bi-chevron-right"></i>
+            <i className='bi bi-chevron-right'></i>
           </button>
         </div>
-        <div className="mobile-copyright-column">
+        <div className='mobile-copyright-column'>
           <p>
             © MANGROVE FOODS LTD {new Date().getFullYear()} - ALL RIGHTS
             RESERVED
           </p>
-          <div className="social-media-icons">
+          <div className='social-media-icons'>
             <a
-              href="https://www.facebook.com/mangrovefoods"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://www.facebook.com/mangrovefoods'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <i className="bi bi-facebook"></i>
+              <i className='bi bi-facebook'></i>
             </a>
             <a
-              href="https://www.instagram.com/mangrovefoods"
-              target="_blank"
-              rel="noopener noreferrer"
+              href='https://www.instagram.com/mangrovefoods'
+              target='_blank'
+              rel='noopener noreferrer'
             >
-              <i className="bi bi-instagram"></i>
+              <i className='bi bi-instagram'></i>
             </a>
           </div>
         </div>
@@ -97,38 +98,38 @@ export function Footer() {
 
       <img
         src={MangroveBrand}
-        alt="Mangrove Brand"
-        className="img-fluid mangrove-brand"
+        alt='Mangrove Brand'
+        className='img-fluid mangrove-brand'
       />
 
-      <div className="footer-bottom">
+      <div className='footer-bottom'>
         <div></div>
         <div></div>
         <p>
           © MANGROVE FOODS LTD {new Date().getFullYear()} - ALL RIGHTS RESERVED
         </p>
         <div />
-        <div className="social-media-icons">
+        <div className='social-media-icons'>
           <a
-            href="https://www.facebook.com/mangrovefoods"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://www.facebook.com/mangrovefoods'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            <i className="bi bi-facebook"></i>
+            <i className='bi bi-facebook'></i>
           </a>
           <a
-            href="https://www.instagram.com/mangrovefoods"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://www.instagram.com/mangrovefoods'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            <i className="bi bi-instagram"></i>
+            <i className='bi bi-instagram'></i>
           </a>
           <a
-            href="https://www.tiktok.com/mangrovefoods"
-            target="_blank"
-            rel="noopener noreferrer"
+            href='https://www.tiktok.com/mangrovefoods'
+            target='_blank'
+            rel='noopener noreferrer'
           >
-            <i className="bi bi-tiktok"></i>
+            <i className='bi bi-tiktok'></i>
           </a>
         </div>
         <div />
