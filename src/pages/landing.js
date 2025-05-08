1192493bf3175939a03fd62a76d3c4452c668d0b
Excellence D. <eoyeniran@tradedepot.co>
2025-05-09 00:32:46 +0100

    fix: style fix

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -36,8 +36,8 @@ export function LandingPage() {
     },
     tablet: {
       breakpoint: { max: 1024, min: 464 },
-      items: 2,
-      slidesToSlide: 2, // optional, default to 1.
+      items: 3,
+      slidesToSlide: 3, // optional, default to 1.
     },
     mobile: {
       breakpoint: { max: 464, min: 0 },
